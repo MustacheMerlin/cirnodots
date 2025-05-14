08e42a8a5d8510653d0d7d90cb09225bb6a2dc6b
Lynn <03sdd18@gmail.com>
2025-05-14 14:38:32 -0600

    Sanitize string to make NIXOS_LABEL work in grub list

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,9 @@
 TODAY=$(date '+%a %F')
 GIT_MESSAGE=$(git log -1 --pretty=%s | cut -c -80)
-sudo NIXOS_LABEL_VERSION="NIXOS $TODAY $GIT_MESSAGE" nixos-rebuild switch --flake .#cirno
+export NIXOS_LABEL="CIRNO NIXOS $TODAY....$GIT_MESSAGE"
+export NIXOS_LABEL=$NIXOS_LABEL
+export NIXOS_LABEL="${NIXOS_LABEL// /_}"
+export NIXOS_LABEL="${NIXOS_LABEL//[^a-zA-Z0-9_:\-\.]/}"
+export NIXOS_LABEL_VERSION=$NIXOS_LABEL
+sudo nixos-rebuild switch --flake .#cirno
+
