d33aafdd2d5783254520d84ebb27d572620ce107
Lynn <03sdd18@gmail.com>
2025-05-14 14:08:33 -0600

    Disable random rocm installation attempts

--- configuration.nix
+++ configuration.nix
@@ -34,13 +34,13 @@
   boot.kernelParams = [ "split_lock_detect=off" ];
   boot.kernelModules = ["amdgpu"];
 
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-  ];
+  #systemd.tmpfiles.rules = [
+    #"L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+  #];
 
-  hardware.graphics.extraPackages = with pkgs; [
-    rocmPackages.clr.icd
-  ];
+  #hardware.graphics.extraPackages = with pkgs; [
+    #rocmPackages.clr.icd
+  #];
   #environment.variables.AMD_VULKAN_ICD = "RADV";
   environment.variables.__NV_PRIME_RENDER_OFFLOAD = 0;
 
