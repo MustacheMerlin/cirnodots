902b83b8a9d752611c5d9f055b9071afe8b313dd
Lynn <03sdd18@gmail.com>
2025-07-04 16:33:34 -0600

    rootless docker, lynn no longer root equivalent docker group, add nvidia cuda docker

--- modules/docker.nix
+++ modules/docker.nix
@@ -2,19 +2,20 @@
 
 {
   virtualisation.docker.enable = true;
-  users.extraGroups.docker.members = [ "lynn" ];
-#  virtualisation.docker.rootless = {
-#    enable = true;
-#    setSocketVariable = true;
-#  }; # enable with systemctl --user enable --now docker and check with systemctl --user status docker
-#
-#  virtualisation.docker.daemon.settings = {
-#    userland-proxy = false;
-#    experimental = true;
-#    metrics-addr = "0.0.0.0:9323";
-#    ipv6 = true;
-#    fixed-cidr-v6 = "fd00::/80";
-#    data-root = "/mnt/bigchungus/docker";
-#  };
+  #users.extraGroups.docker.members = [ "lynn" ]; # security hole, docker group is equivalent to being root.
+  hardware.nvidia-container-toolkit.enable = true;
+   virtualisation.docker.rootless = {
+     enable = true;
+     setSocketVariable = true;
+   }; # enable with systemctl --user enable --now docker and check with systemctl --user status docker
+ 
+   virtualisation.docker.daemon.settings = {
+     userland-proxy = false;
+     experimental = true;
+     metrics-addr = "0.0.0.0:9323";
+     ipv6 = true;
+     fixed-cidr-v6 = "fd00::/80";
+     data-root = "/mnt/bigchungus/docker";
+   };
 
 }
